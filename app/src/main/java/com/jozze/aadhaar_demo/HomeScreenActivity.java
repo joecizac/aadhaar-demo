eb6198a8d918ab4b2131bceb22194f4833e5f7a6
sharad <sharad@creativemoments.lk>
2016-10-15 03:11:24 -0700

    UI

--- app/src/main/java/com/jozze/aadhaar_demo/HomeScreenActivity.java
+++ app/src/main/java/com/jozze/aadhaar_demo/HomeScreenActivity.java
@@ -4,13 +4,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     private RelativeLayout setImage;
     private LinearLayout meLayout;
+    private ImageView iv_profile;
+    private TextView tv_name;
+    private TextView tv_dob;
+    private TextView tv_gender;
+    private TextView tv_add1;
+    private TextView tv_add2;
+    private LinearLayout ll_home;
+    private LinearLayout ll_doctor;
+    private LinearLayout ll_social;
+    private LinearLayout ll_me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +31,13 @@ public class HomeScreenActivity extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home_screen);
-        setImage = (RelativeLayout) findViewById(R.id.setImage);
-        meLayout = (LinearLayout) findViewById(R.id.meLayout);
-        LinearLayout ll_home = (LinearLayout) findViewById(R.id.ll_home);
-        LinearLayout ll_doctor = (LinearLayout) findViewById(R.id.ll_doctor);
-        LinearLayout ll_social = (LinearLayout) findViewById(R.id.ll_social);
-        LinearLayout ll_me = (LinearLayout) findViewById(R.id.ll_me);
+        initViews();
+//        setData();
+        onclickListners();
 
+    }
 
+    private void onclickListners() {
         ll_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,6 +70,21 @@ public class HomeScreenActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initViews() {
+        iv_profile = (ImageView) findViewById(R.id.iv_profile);
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        tv_dob = (TextView) findViewById(R.id.tv_dob);
+        tv_gender = (TextView) findViewById(R.id.tv_gender);
+        tv_add1 = (TextView) findViewById(R.id.tv_add1);
+        tv_add2 = (TextView) findViewById(R.id.tv_add2);
+        setImage = (RelativeLayout) findViewById(R.id.setImage);
+        meLayout = (LinearLayout) findViewById(R.id.meLayout);
+        ll_home = (LinearLayout) findViewById(R.id.ll_home);
+        ll_doctor = (LinearLayout) findViewById(R.id.ll_doctor);
+        ll_social = (LinearLayout) findViewById(R.id.ll_social);
+        ll_me = (LinearLayout) findViewById(R.id.ll_me);
     }
 
 
