c36fa5d05ecab015053a911a3b909662d9348741
sharad <sharad@creativemoments.lk>
2016-10-15 05:23:49 -0700

    UI

--- app/src/main/java/com/jozze/aadhaar_demo/AadhaarActivity.java
+++ app/src/main/java/com/jozze/aadhaar_demo/AadhaarActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -27,6 +28,8 @@ public class AadhaarActivity extends AppCompatActivity implements NetworkRespons
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_aadhaar);
         Button submit = (Button) findViewById(R.id.submit);
         otpEditText = (EditText) findViewById(R.id.otpEditText);
--- app/src/main/java/com/jozze/aadhaar_demo/HomeScreenActivity.java
+++ app/src/main/java/com/jozze/aadhaar_demo/HomeScreenActivity.java
@@ -2,6 +2,7 @@ package com.jozze.aadhaar_demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.jozze.aadhaar_demo.models.KYC;
 
@@ -22,11 +24,11 @@ public class HomeScreenActivity extends AppCompatActivity {
     private TextView tv_dob;
     private TextView tv_gender;
     private TextView tv_add1;
-    private TextView tv_add2;
     private LinearLayout ll_home;
     private LinearLayout ll_doctor;
     private LinearLayout ll_social;
     private LinearLayout ll_me;
+    private TextView tv_pincode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,10 +81,10 @@ public class HomeScreenActivity extends AppCompatActivity {
     private void initViews() {
         iv_profile = (ImageView) findViewById(R.id.iv_profile);
         tv_name = (TextView) findViewById(R.id.tv_name);
+        tv_pincode = (TextView) findViewById(R.id.tv_pincode);
         tv_dob = (TextView) findViewById(R.id.tv_dob);
         tv_gender = (TextView) findViewById(R.id.tv_gender);
         tv_add1 = (TextView) findViewById(R.id.tv_add1);
-        tv_add2 = (TextView) findViewById(R.id.tv_add2);
         setImage = (RelativeLayout) findViewById(R.id.setImage);
         meLayout = (LinearLayout) findViewById(R.id.meLayout);
         ll_home = (LinearLayout) findViewById(R.id.ll_home);
@@ -92,6 +94,29 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         KYC kyc = new Gson().fromJson(getIntent().getStringExtra("Data"), KYC.class);
         tv_name.setText(kyc.getKyc().getPersonal().getName());
+        tv_dob.setText(kyc.getKyc().getPersonal().getBirthDate());
+        tv_gender.setText(kyc.getKyc().getPersonal().getGender());
+        tv_pincode.setText(kyc.getKyc().getAddress().getPincode());
+        String s="";
+        if (kyc.getKyc().getAddress().getCareOf()!=null)
+        s= s+kyc.getKyc().getAddress().getCareOf() + ", " + "\n";
+
+        if (kyc.getKyc().getAddress().getArea()!=null)
+            s=s+ kyc.getKyc().getAddress().getArea() + ", " + "\n";
+        if (kyc.getKyc().getAddress().getStreet()!=null)
+            s= s+kyc.getKyc().getAddress().getStreet() + ", " + "\n";
+        if (kyc.getKyc().getAddress().getPostOffice()!=null)
+            s= s+kyc.getKyc().getAddress().getPostOffice() + ", " + "\n";
+        if (kyc.getKyc().getAddress().getDistrict()!=null)
+            s= s+kyc.getKyc().getAddress().getDistrict() + ", " + "\n";
+        if (kyc.getKyc().getAddress().getState()!=null)
+            s= s+kyc.getKyc().getAddress().getState() ;
+
+        tv_add1.setText(s);
+
+        String imageString = kyc.getKyc().getImageByteArray().replaceAll("\r", "").replaceAll("\n", "");
+        byte[] imageByteArray = Base64.decode(imageString, Base64.DEFAULT);
+        Glide.with(HomeScreenActivity.this).load(imageByteArray).asBitmap().placeholder(R.drawable.logo).into(iv_profile);
     }
 
 
